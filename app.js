a8c1a2087d2c61d9503f0ef437a3b5e782edcb28
aravind.emmadishetty <aravind.emmadishetty@acsicorp.com>
2022-01-08 13:00:50 +0530

    updates

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/book_library';
+var dev_db_url = 'mongodb://127.0.0.1/book_library';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
